37743003d569e54145df1ee4e8ad0a68de216638
github_yjqiang <8900942+yjqiang@users.noreply.github.com>
2021-07-28 00:22:54 +0800

    允许 key 可以不出现
    即 filter.toml 中 sort_keys 可以允许在 0.conf 中缺失

--- parse.py
+++ parse.py
@@ -28,7 +28,8 @@ def write(conf: dict, path: str, sort_keys: list) -> None:
     with open(path, 'w', encoding='utf8') as f:
         for key in sort_keys:
             f.write(f'{key}\n')
-            for line in conf[key]:
-                f.write(f'{line}\n')
+            if key in conf:
+                for line in conf[key]:
+                    f.write(f'{line}\n')
             f.write('\n')
 
